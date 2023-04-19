563b0a0fe088b9c9c5766b56b16973fe8e58be0c
devdooms <116958892+domingodominic@users.noreply.github.com>
2023-04-19 16:36:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// Add event listener to the window scroll event
+
+const scrollToTopButton = document.getElementById("scroll-to-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 0) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+});
+
+// Add click event listener to the scroll-to-top button
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
